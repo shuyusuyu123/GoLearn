21392c058f6f89f49abf28bf5a12b27cbab0ada0
yshu <yshu@yuansuan.cn>
2022-04-01 18:06:13 +0800

    calc文件夹调整

--- calc/simplemath/sqrt.go
+++ calc/simplemath/sqrt.go
@@ -2,7 +2,7 @@ package simplemath
 
 import "math"
 
-func Sqrt(i int)int{
-	v :=math.Sqrt(float64(i))
+func Sqrt(i int) int {
+	v := math.Sqrt(float64(i))
 	return int(v)
 }
--- calc/simplemath/sqrt_test.go
+++ calc/simplemath/sqrt_test.go
@@ -2,9 +2,9 @@ package simplemath
 
 import "testing"
 
-func TestSqrt1(t *testing.T){
-	v :=Sqrt(16)
-	if v!=4{
-		t.Errorf("Sqrt(16) filed.Got %v,expected 4.",v)
+func TestSqrt1(t *testing.T) {
+	v := Sqrt(16)
+	if v != 4 {
+		t.Errorf("Sqrt(16) filed.Got %v,expected 4.", v)
 	}
 }
