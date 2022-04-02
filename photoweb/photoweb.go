c5f727ea0bdbccbf7522dd7075b34f2d63f8afa9
yshu <yshu@yuansuan.cn>
2022-04-02 15:55:59 +0800

    简单的相册网站

---
+++ photoweb/photoweb.go
@@ -0,0 +1,167 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"path"
+	"runtime/debug"
+)
+
+const (
+	ListDir      = 0x0001
+	UPLOAD_DIR   = "./uploads"
+	TEMPLATE_DIR = "./views"
+)
+
+//模板缓存——书上写法错误templates := make(map[string]*template.Template)
+var templates map[string]*template.Template = make(map[string]*template.Template)
+
+//加载所有模板
+func init() {
+	fileInfoArr, err := ioutil.ReadDir(TEMPLATE_DIR)
+	check(err)
+	var templateName, templatePath string
+	for _, fileInfo := range fileInfoArr {
+		templateName = fileInfo.Name()
+		if ext := path.Ext(templateName); ext != ".html" {
+			continue
+		}
+		templatePath = TEMPLATE_DIR + "/" + templateName
+		log.Println("Loading template:", templatePath)
+		t := template.Must(template.ParseFiles(templatePath))
+		rlname := realName(templateName)
+		log.Println("Loading template:", rlname)
+		templates[rlname] = t
+	}
+}
+
+//获取名称
+func realName(str string) string {
+	str = path.Base(str)
+	if str == "" {
+		return str
+	}
+	for i := 0; i < len(str); i++ {
+		if '.' == str[i] {
+			return str[:i]
+		}
+	}
+	return str
+}
+
+//错误处理
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+//渲染模板
+func renderHtml(w http.ResponseWriter, tmpl string, locals map[string]interface{}) {
+	err := templates[tmpl].Execute(w, locals)
+	check(err)
+}
+
+//处理不存在的图片访问
+func isExists(path string) bool {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true
+	}
+	return os.IsExist(err)
+}
+
+//上传图片
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		renderHtml(w, "upload", nil)
+	}
+	if r.Method == "POST" {
+		f, h, err := r.FormFile("image")
+		check(err)
+		filename := h.Filename
+		defer f.Close()
+		t, err := ioutil.TempFile(UPLOAD_DIR, filename)
+		check(err)
+		defer t.Close()
+		_, err = io.Copy(t, f)
+		check(err)
+		http.Redirect(w, r, "/view?id="+filename,
+			http.StatusFound)
+	}
+}
+
+//在网页上显示图片
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	imageId := r.FormValue("id")
+	imagePath := UPLOAD_DIR + "/" + imageId
+	if exists := isExists(imagePath); !exists {
+		http.NotFound(w, r)
+		return
+	}
+	w.Header().Set("Content-Type", "image")
+	http.ServeFile(w, r, imagePath)
+}
+
+//巧用闭包避免程序运行时出错崩溃
+func safeHandler(fn http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if e, ok := recover().(error); ok {
+				http.Error(w, e.Error(), http.StatusInternalServerError)
+				// 或者输出自定义的50x错误页面
+				// w.WriteHeader(http.StatusInternalServerError)
+				// renderHtml(w, "error", e)
+				// logging
+				log.Println("WARN: panic in %v. - %v", fn, e)
+				log.Println(string(debug.Stack()))
+			}
+		}()
+		fn(w, r)
+	}
+}
+
+//列出所有已上传图片
+func listHandler(w http.ResponseWriter, r *http.Request) {
+	fileInfoArr, err := ioutil.ReadDir("./uploads")
+	check(err)
+	locals := make(map[string]interface{})
+	images := []string{}
+	for _, fileInfo := range fileInfoArr {
+		images = append(images, fileInfo.Name())
+	}
+	locals["images"] = images
+	renderHtml(w, "list", locals)
+}
+
+//动态请求和静态资源分离
+func staticDirHandler(mux *http.ServeMux, prefix string, staticDir string, flags int) {
+	mux.HandleFunc(prefix, func(w http.ResponseWriter, r *http.Request) {
+		file := staticDir + r.URL.Path[len(prefix)-1:]
+		if (flags & ListDir) == 0 {
+			if exists := isExists(file); !exists {
+				http.NotFound(w, r)
+				fmt.Println(file, "not found")
+				return
+			}
+		}
+		fmt.Println("handle static dir")
+		http.ServeFile(w, r, file)
+	})
+}
+func main() {
+	mux := http.NewServeMux()
+	staticDirHandler(mux, "/assets/", "./public", 0)
+	mux.HandleFunc("/", safeHandler(listHandler))
+	mux.HandleFunc("/view", safeHandler(viewHandler))
+	mux.HandleFunc("/upload", safeHandler(uploadHandler))
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err.Error())
+	}
+}
